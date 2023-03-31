7f8bf743a53fe717a5e80491d1205b4c4ff3acce
Tore Aursand <toreau@gmail.com>
2023-03-31 10:18:48 +0200

    Initial commit.

---
+++ lib/Postnr.pm
@@ -0,0 +1,57 @@
+package Postnr;
+use Mojo::Base 'Mojolicious', -signatures;
+
+use Mojo::Util qw( trim );
+
+# This method will run once at server start
+sub startup ($self) {
+  # Load configuration from config file
+  my $config = $self->plugin( 'NotYAMLConfig' );
+
+  # Configure the application
+  $self->secrets( $config->{'secrets'} );
+
+  # Prepare data
+  $self->_prepare_data;
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get( '/:postnr' )->to( 'Lookup#index' );
+
+  # Done?
+  if ( $self->postnr->{'7633'} ) {
+    $self->log->debug( 'Application started successfully!' );
+  }
+  else {
+    $self->log->error( 'Failed to start application because of missing data!' );
+    die;
+  }
+}
+
+# Creates a helper out of Bring's data
+sub _prepare_data ($self) {
+  my $data  = $self->ua->get( 'https://www.bring.no/postnummerregister-ansi.txt' )->res->body;
+  my @lines = map { trim($_) } split( /\n+/, $data );
+
+  my %postnr = ();
+
+  foreach ( @lines ) {
+    my @fields = split( /\t+/, $_ );
+
+    $postnr{ $fields[0] } = {
+      'postnummer'  => $fields[0],
+      'poststed'    => $fields[1],
+      'kommunekode' => $fields[2],
+      'kommunenavn' => $fields[3],
+      'kategori'    => $fields[4],
+    };
+  }
+
+  $self->helper( 'postnr' => sub ($self) {
+    return \%postnr;
+  } );
+}
+
+1;
---
+++ lib/Postnr/Controller/Lookup.pm
@@ -0,0 +1,30 @@
+package Postnr::Controller::Lookup;
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+
+sub index ($self) {
+  my $postnr = $self->param( 'postnr' ) // '';
+
+  unless ( $postnr =~ m/^(\d\d\d\d)$/ ) {
+    return $self->_render_error( 400, 'Bad request; input parameter must be 4 digits long!' );
+  }
+
+  if ( my $data = $self->postnr->{$postnr} ) {
+    return $self->render(
+      'json' => $data,
+    );
+  }
+  else {
+    return $self->_render_error( 404, $postnr . ' was not found!' );
+  }
+}
+
+sub _render_error ($self, $status_code, $error) {
+  return $self->render(
+    'status' => $status_code,
+    'json'   => {
+      'error' => $error // 'Unknown error',
+    },
+  );
+}
+
+1;
