99fb111d8db7fd580631a7a3fc6f0d958bd9c45a
Tore Aursand <toreau@gmail.com>
2023-03-31 12:25:35 +0200

    Improved code readability slightly.

--- lib/Postnr/Controller/Lookup.pm
+++ lib/Postnr/Controller/Lookup.pm
@@ -8,14 +8,15 @@ sub index ($self) {
     return $self->_render_error( 400, 'Bad request; input parameter must be 4 digits long!' );
   }
 
-  if ( my $data = $self->postnr->{$postnr} ) {
-    return $self->render(
-      'json' => $data,
-    );
-  }
-  else {
+  my $data = $self->postnr->{$postnr};
+  unless ( $data ) {
     return $self->_render_error( 404, $postnr . ' was not found!' );
   }
+
+  return $self->render(
+    'status' => 200,
+    'json'   => $data,
+  );
 }
 
 sub _render_error ($self, $status_code, $error) {
